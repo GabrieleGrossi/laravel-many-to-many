e84f974fdb40d4148d9d0af839dfd98c0ce43ed5
GabrieleGrossi <grossigabriele98@gmail.com>
2023-08-29 15:36:56 +0200

    fix the login section

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -100,9 +100,9 @@
                             <label for="reg-log"></label>
                         <div class="card-3d-wrap mx-auto">
                             <form method="POST" action="{{ route('login') }}">
+                                <div class="card-3d-wrapper">
                                 @csrf
                                 
-                                <div class="card-3d-wrapper">
                                     <div class="card-front">
                                         <div class="center-wrap">
                                             <div class="section text-center mc">
@@ -119,40 +119,12 @@
                                                     @enderror
                                                 </div>	
                                                 <div class="form-group mt-2">
-                                                    <input type="password" class="form-style @error('password) is-invalid @enderror" placeholder="Password" id="password" name="password" value="{{old ('password')}}" required>
+                                                    <input type="password" class="form-style @error('password') is-invalid @enderror" placeholder="Password" id="password" name="password" value="{{old ('password')}}" required>
                                                     <i class="input-icon uil uil-lock-alt"></i>
                                                 </div>
                                                 <button type="submit" class="link btn">
                                                     {{ __('Login') }}
                                                 </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card-back mc">
-                                        <div class="center-wrap">
-                                            <div class="section text-center">
-                                                <h4 class="mb-3 pb-3">
-                                                    Sign Up
-                                                </h4>
-                                                <div class="form-group">
-                                                    <input type="text" class="form-style" placeholder="Full Name">
-                                                    <i class="input-icon uil uil-user"></i>
-                                                </div>	
-                                                <div class="form-group mt-2">
-                                                    <input type="tel" class="form-style" placeholder="Phone Number">
-                                                    <i class="input-icon uil uil-phone"></i>
-                                                </div>	
-                                                <div class="form-group mt-2">
-                                                    <input type="email" class="form-style" placeholder="Email">
-                                                    <i class="input-icon uil uil-at"></i>
-                                                </div>
-                                                <div class="form-group mt-2">
-                                                    <input type="password" class="form-style" placeholder="Password">
-                                                    <i class="input-icon uil uil-lock-alt"></i>
-                                                </div>
-                                                <a href="https://www.web-leb.com/code" class="btn mt-4">
-                                                    Register
-                                                </a>
                                             </div>
                                         </div>
                                     </div>
@@ -166,8 +138,4 @@
     </div>
 </body>
 </html>
-
-
-
-
 @endsection
