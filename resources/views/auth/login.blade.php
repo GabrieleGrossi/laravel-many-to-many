fbe5e3ab0f50a0e08598d9d4811c34bc46f155c2
GabrieleGrossi <grossigabriele98@gmail.com>
2023-08-29 12:49:41 +0200

    Modified my login.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -76,7 +76,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <title>Webleb</title>
+  <title>Boolpress</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
@@ -99,61 +99,65 @@
                             <input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
                             <label for="reg-log"></label>
                         <div class="card-3d-wrap mx-auto">
-                            <div class="card-3d-wrapper">
-                                <div class="card-front">
-                                    <div class="center-wrap">
-                                        <div class="section text-center">
-                                            <h4 class="mb-4 pb-3 mc">
-                                                Log In
-                                            </h4>
-                                            <div class="form-group">
-                                                <input type="email" class="form-style" placeholder="Email">
-                                                <i class="input-icon uil uil-at"></i>
-                                            </div>	
-                                            <div class="form-group mt-2">
-                                                <input type="password" class="form-style" placeholder="Password">
-                                                <i class="input-icon uil uil-lock-alt"></i>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                
+                                <div class="card-3d-wrapper">
+                                    <div class="card-front">
+                                        <div class="center-wrap">
+                                            <div class="section text-center mc">
+                                                <h4 class="mb-4 pb-3">
+                                                    Log In
+                                                </h4>
+                                                <div class="form-group mt-2">
+                                                    <input type="email" class="form-style @error('email') is-invalid @enderror" placeholder="Email" id="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                                    <i class="input-icon uil uil-at"></i>
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>	
+                                                <div class="form-group mt-2">
+                                                    <input type="password" class="form-style @error('password) is-invalid @enderror" placeholder="Password" id="password" name="password" value="{{old ('password')}}" required>
+                                                    <i class="input-icon uil uil-lock-alt"></i>
+                                                </div>
+                                                <button type="submit" class="link btn">
+                                                    {{ __('Login') }}
+                                                </button>
                                             </div>
-                                            <a href="https://www.web-leb.com/code" class="btn mt-4">
-                                                Login
-                                            </a>
-                                            <p class="mb-0 mt-4 text-center">
-                                                <a href="https://www.web-leb.com/code" class="link">
-                                                    Forgot your password?
+                                        </div>
+                                    </div>
+                                    <div class="card-back mc">
+                                        <div class="center-wrap">
+                                            <div class="section text-center">
+                                                <h4 class="mb-3 pb-3">
+                                                    Sign Up
+                                                </h4>
+                                                <div class="form-group">
+                                                    <input type="text" class="form-style" placeholder="Full Name">
+                                                    <i class="input-icon uil uil-user"></i>
+                                                </div>	
+                                                <div class="form-group mt-2">
+                                                    <input type="tel" class="form-style" placeholder="Phone Number">
+                                                    <i class="input-icon uil uil-phone"></i>
+                                                </div>	
+                                                <div class="form-group mt-2">
+                                                    <input type="email" class="form-style" placeholder="Email">
+                                                    <i class="input-icon uil uil-at"></i>
+                                                </div>
+                                                <div class="form-group mt-2">
+                                                    <input type="password" class="form-style" placeholder="Password">
+                                                    <i class="input-icon uil uil-lock-alt"></i>
+                                                </div>
+                                                <a href="https://www.web-leb.com/code" class="btn mt-4">
+                                                    Register
                                                 </a>
-                                            </p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-back mc">
-                                    <div class="center-wrap">
-                                        <div class="section text-center">
-                                            <h4 class="mb-3 pb-3">
-                                                Sign Up
-                                            </h4>
-                                            <div class="form-group">
-                                                <input type="text" class="form-style" placeholder="Full Name">
-                                                <i class="input-icon uil uil-user"></i>
-                                            </div>	
-                                            <div class="form-group mt-2">
-                                                <input type="tel" class="form-style" placeholder="Phone Number">
-                                                <i class="input-icon uil uil-phone"></i>
-                                            </div>	
-                                            <div class="form-group mt-2">
-                                                <input type="email" class="form-style" placeholder="Email">
-                                                <i class="input-icon uil uil-at"></i>
-                                            </div>
-                                            <div class="form-group mt-2">
-                                                <input type="password" class="form-style" placeholder="Password">
-                                                <i class="input-icon uil uil-lock-alt"></i>
-                                            </div>
-                                            <a href="https://www.web-leb.com/code" class="btn mt-4">
-                                                Register
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
