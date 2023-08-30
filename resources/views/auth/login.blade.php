21046f8b8bf2e29d0a43da735359c1c51a51f3f4
GabrieleGrossi <grossigabriele98@gmail.com>
2023-08-30 15:01:26 +0200

    return to the old version of login because the newest cause generic problem

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- 
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -71,71 +71,4 @@
         </div>
     </div>
 </div>
--->
-
-<!doctype html>
-<html lang="en">
-<head>
-  <title>Boolpress</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="/scss/_variable.scss">
-</head>
-<body>
-    <div id="stars"></div>
-    <div id="stars2"></div>
-    <div id="stars3"></div>
-    <div class="section">
-        <div class="container">
-            <div class="row full-height justify-content-center">
-                <div class="col-12 text-center align-self-center py-5">
-                    <div class="section pb-5 pt-5 pt-sm-2 text-center">
-                        <h6 class="mb-0 pb-3 mc">
-                            <span>Log In </span>
-                            <span>Sign Up</span>
-                        </h6>
-                            <input class="checkbox" type="checkbox" id="reg-log" name="reg-log"/>
-                            <label for="reg-log"></label>
-                        <div class="card-3d-wrap mx-auto">
-                            <form method="POST" action="{{ route('login') }}">
-                                <div class="card-3d-wrapper">
-                                @csrf
-                                
-                                    <div class="card-front">
-                                        <div class="center-wrap">
-                                            <div class="section text-center mc">
-                                                <h4 class="mb-4 pb-3">
-                                                    Log In
-                                                </h4>
-                                                <div class="form-group mt-2">
-                                                    <input type="email" class="form-style @error('email') is-invalid @enderror" placeholder="Email" id="email" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                                    <i class="input-icon uil uil-at"></i>
-                                                    @error('email')
-                                                        <span class="invalid-feedback" role="alert">
-                                                            <strong>{{ $message }}</strong>
-                                                        </span>
-                                                    @enderror
-                                                </div>	
-                                                <div class="form-group mt-2">
-                                                    <input type="password" class="form-style @error('password') is-invalid @enderror" placeholder="Password" id="password" name="password" value="{{old ('password')}}" required>
-                                                    <i class="input-icon uil uil-lock-alt"></i>
-                                                </div>
-                                                <button type="submit" class="link btn">
-                                                    {{ __('Login') }}
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
 @endsection
